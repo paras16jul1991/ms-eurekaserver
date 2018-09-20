5a55f483c9fe99ae9385589928f8d2cd4a41441a
parass <paras.sharma@bitwiseglobal.com>
2018-09-20 19:07:21 +0530

    init changes added.

---
+++ src/main/java/com/ms/managment/system/mseurekaserver/MsEurekaserverApplication.java
@@ -0,0 +1,14 @@
+package com.ms.managment.system.mseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MsEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaserverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ms/managment/system/mseurekaserver/MsEurekaserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ms.managment.system.mseurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsEurekaserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
